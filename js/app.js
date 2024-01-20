34e7cfcd6031330b21a4aaa5e945e07d0da6c8a0
Teejay31 <tijaniidris2020@gmail.com>
2024-01-20 19:10:44 +0100

    update

--- js/app.js
+++ js/app.js
@@ -17,3 +17,16 @@ window.addEventListener('scroll', () => {
 //     document.style.display = "block";
 // })
 
+// document.onreadystatechange = function() {
+//     if (document.readyState !== "complete") {
+//         document.querySelector(
+//             "body").style.visibility = "hidden";
+//             document.querySelector(
+//             "#loader").style.visibility = "visible";
+//     } else {
+//         document.querySelector(
+//         "#loader").style.display = "none";
+//         document.querySelector(
+//         "body").style.visibility = "visible"
+//     }
+// };
